db5cad757d37831c1d26507e4390ac95f1ce546b
Daniel YU <i@icymint.me>
2019-04-04 21:13:57 +0800

    restrict yaml support

--- salak-yaml/src/Salak/Yaml.hs
+++ salak-yaml/src/Salak/Yaml.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards  #-}
 {-# LANGUAGE TupleSections    #-}
+-- |
+-- Module:      Salak.Yaml
+-- Copyright:   (c) 2019 Daniel YU
+-- License:     BSD3
+-- Maintainer:  leptonyu@gmail.com
+-- Stability:   experimental
+-- Portability: portable
+--
+-- Yaml support for "Salak". Yaml alias and anchor is not supported.
+--
 module Salak.Yaml(
     YAML(..)
   , loadYaml
@@ -14,9 +24,11 @@ import           Salak
 import           Salak.Load
 import           Text.Libyaml
 
+-- | Load Yaml
 loadYaml :: MonadIO m => FilePath -> LoadSalakT m ()
 loadYaml file = loadFile file $ \i s -> liftIO $ runConduitRes (decodeFileMarked file .| loadYAML i s)
 
+-- | YAML notation for `loadYaml`
 data YAML = YAML
 
 instance HasLoad YAML where
@@ -29,6 +41,7 @@ loadYAML i s = await >>= maybe (return s) go
     go (MarkedEvent EventSequenceStart{}  _ _) = goSeq 0 s
     go (MarkedEvent EventSequenceEnd      _ _) = return emptySource
     go (MarkedEvent EventMappingStart{}  _ ee) = goMap ee s
+    go (MarkedEvent (EventAlias a)       _ ee) = ge ee $ "alias " ++ a ++ " not supported by salak"
     go _ = loadYAML i s
     goSeq j s1 = do
       s' <- loadYAML i emptySource
@@ -42,7 +55,7 @@ loadYAML i s = await >>= maybe (return s) go
         Just (MarkedEvent (EventScalar a _ _ _) _ ee') ->
           updateSources (simpleSelectors $ decodeUtf8 a) (loadYAML i) s1 >>= goMap ee'
         Just (MarkedEvent EventMappingEnd _ _) -> return s1
-        Just e -> ge (yamlStartMark e) "suppose scalar and mapping end"
+        Just e -> ge (yamlStartMark e) ("suppose scalar and mapping end, but is " ++ show (yamlEvent e))
     ge YamlMark{..} e = liftIO $ throwIO $ YamlException $ "(" ++ show yamlLine ++ "," ++ show yamlColumn ++ ")" ++ e
 
 
