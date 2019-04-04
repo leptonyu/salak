f7f36c039d051cfd0d2188b50ed6c94ba64398a3
Daniel YU <i@icymint.me>
2019-04-04 11:59:12 +0800

    refact toml

--- salak-toml/src/Salak/Toml.hs
+++ salak-toml/src/Salak/Toml.hs
@@ -30,31 +30,30 @@ toS :: Piece -> Selector
 toS = SStr . unPiece
 
 loadTOML :: Monad m => T.TOML -> Priority -> Source -> WriterT [String] m Source
-loadTOML T.TOML{..} i s = HM.foldlWithKey' (g1 i) (return s) tomlPairs
-                        >>= h2 i tomlTables
+loadTOML T.TOML{..} i = foldPairs       i tomlPairs
+                    >=> foldTables      i tomlTables
+                    >=> foldTableArrays i tomlTableArrays
+foldToml go m s = HM.foldlWithKey' (\ms k v -> ms >>= go k v) (return s) m
+foldPairs  i      = foldToml (\k -> updateSources (toSs k) . insertAnyValue i)
+foldTables i      = foldToml (const go)
+  where
+    go (Leaf   k     toml) = updateSources (toSs k) (loadTOML toml i)
+    go (Branch k v' tomap) = updateSources (toSs k) (maybe return (`loadTOML` i) v' >=> foldTables i tomap)
+foldTableArrays i = foldToml (\k v -> updateSources (toSs k) (foldArray (N.toList v) (`loadTOML` i)))
 
-g1 i ms k v' = ms >>= updateSources (toSs k) (f1 i v')
-g2 i ms _ v' = ms >>= f2 i v'
+insertAnyValue :: Monad m => Priority -> AnyValue -> Source -> m Source
+insertAnyValue i (AnyValue (Array   b))             = foldArray b (insertAnyValue i . AnyValue)
+insertAnyValue i (AnyValue (Bool    b))             = return . insertSource (VBool  i b)
+insertAnyValue i (AnyValue (Integer b))             = return . insertSource (VNum   i $ fromIntegral b)
+insertAnyValue i (AnyValue (Double  b))             = return . insertSource (VNum   i $ realToFrac b)
+insertAnyValue i (AnyValue (Text    b))             = return . insertSource (VStr   i b)
+insertAnyValue i (AnyValue (Local   b))             = return . insertSource (VLTime i b)
+insertAnyValue i (AnyValue (Day     b))             = return . insertSource (VDay   i b)
+insertAnyValue i (AnyValue (Hours   b))             = return . insertSource (VHour  i b)
+insertAnyValue i (AnyValue (Zoned (ZonedTime a b))) = return . insertSource (VZTime i b a)
 
-f1 :: Monad m => Priority -> AnyValue -> Source -> m Source
-f1 i (AnyValue (Array   b)) = \s -> foldM (\s' (ix,x) -> updateSource (SNum ix) (f1 i $ AnyValue x) s') s $ zip [0..] b
-f1 i (AnyValue (Bool    b)) = return . insertSource (VBool  i b)
-f1 i (AnyValue (Integer b)) = return . insertSource (VNum   i $ fromIntegral b)
-f1 i (AnyValue (Double  b)) = return . insertSource (VNum   i $ realToFrac b)
-f1 i (AnyValue (Text    b)) = return . insertSource (VStr   i b)
-f1 i (AnyValue (Local   b)) = return . insertSource (VLTime i b)
-f1 i (AnyValue (Day     b)) = return . insertSource (VDay   i b)
-f1 i (AnyValue (Hours   b)) = return . insertSource (VHour  i b)
-f1 i (AnyValue (Zoned (ZonedTime a b))) = return . insertSource (VZTime i b a)
--- f1 i _ = return
-f2 :: Monad m => Priority -> PrefixTree T.TOML -> Source -> WriterT [String] m Source
-f2 i (Leaf   k     toml) = updateSources (toSs k) (loadTOML toml i)
-f2 i (Branch k v' tomap) = updateSources (toSs k) (h1 i v' >=> h2 i tomap)
-h1 :: Monad m => Priority -> Maybe T.TOML -> Source -> WriterT [String] m Source
-h1 i (Just t) = loadTOML t i
-h1 _ _        = return
-h2 :: Monad m => Priority -> PrefixMap T.TOML -> Source -> WriterT [String] m Source
-h2 i m s = HM.foldlWithKey' (g2 i) (return s) m
+foldArray :: Monad m => [a] -> (a -> Source -> m Source) -> Source -> m Source
+foldArray a g s = foldM (\s' (ix,x) -> updateSource (SNum ix) (g x) s') s $ zip [0..] a
 
 loadToml :: MonadIO m => FilePath -> LoadSalakT m ()
 loadToml file = do
