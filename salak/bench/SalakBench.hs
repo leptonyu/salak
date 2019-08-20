3b762a3d27921d24e2d69ab7da149274b851387d
Daniel YU <i@icymint.me>
2019-08-20 14:18:04 +0800

    add bench for all

--- salak/bench/SalakBench.hs
+++ salak/bench/SalakBench.hs
@@ -11,20 +11,25 @@ main = do
   sp <- loadAndRunSalak' (loadMock vals) return
   let run :: forall a. ReaderT SourcePack IO a -> IO a
       run = flip runReaderT sp
+  v  <- run $ require "int" :: IO (IO Int)
   defaultMain
     [ bgroup "load"
       [ bench "loadMock" $ whnfIO $ loadAndRunSalak (loadMock vals) action
       , bench "loadCMD"  $ whnfIO $ loadAndRunSalak (loadCommandLine def) action
       , bench "loadEnv"  $ whnfIO $ loadAndRunSalak loadEnv action
       ]
+    , bgroup "parse-io"
+      [ bench "read-io"  $ whnfIO v
+      , bench "parse"    $ whnfIO (run $ require "int" :: IO (IO Int))
+      ]
     , bgroup "parse-int"
-      [ bench "int"      $ whnfIO $ (run $ require "int" :: IO Int)
-      , bench "int/text" $ whnfIO $ (run $ require "int" :: IO Text)
-      , bench "int/bool" $ whnfIO $ (run $ require "int" :: IO (Either String Bool))
+      [ bench "int"      $ whnfIO (run $ require "int" :: IO Int)
+      , bench "int/text" $ whnfIO (run $ require "int" :: IO Text)
+      , bench "int/bool" $ whnfIO (run $ require "int" :: IO (Either String Bool))
       ]
     , bgroup "run"
-      [ bench "text" $ whnfIO $ (run $ require "a.b.c.d.e.f.g.h.i.j.k.text" :: IO Text)
-      , bench "bool" $ whnfIO $ (run $ require "a.b.c.d.e.f.g.h.i.j.k.bool" :: IO Bool)
+      [ bench "text"     $ whnfIO $ (run $ require "a.b.c.d.e.f.g.h.i.j.k.text" :: IO Text)
+      , bench "bool"     $ whnfIO $ (run $ require "a.b.c.d.e.f.g.h.i.j.k.bool" :: IO Bool)
       ]
     ]
 
